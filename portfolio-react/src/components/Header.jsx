e309d99a43284b5058f5e8b1bd09eed7eacd2515
Biswajit Manna <mannabiswajit7046@gmail.com>
2026-02-15 21:03:06 +0530

    Create header component and add it to Home page

---
+++ portfolio-react/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Portfolio</h1>
+
+      <nav className="nav">
+        <ul>
+          <li><a className='active' href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- portfolio-react/src/pages/Home.jsx
+++ portfolio-react/src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import Header from "../components/Header";
+
 function Home () {
   return (
-    <h1>Home</h1>
+    <Header />
   )
 }
 
