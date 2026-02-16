772a76fbf69e41ffcb1aee71ad8a7ed76fa1e0e0
Biswajit Manna <mannabiswajit7046@gmail.com>
2026-02-16 16:44:53 +0530

    add hero section into home page

--- portfolio-react/src/pages/Home.jsx
+++ portfolio-react/src/pages/Home.jsx
@@ -1,8 +1,55 @@
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import Header from "../components/Header";
+import './Home.css';
 
 function Home () {
   return (
-    <Header />
+    <div className="home">
+      <Header />
+
+      <section className="hero-section">
+        <div>
+          <p className="hero-section-greeting">Hi there!</p>
+          <h1 className="hero-section-name">I'm <span>Biswajit Manna</span></h1>
+          <p className="sort-bio">I develop web applications</p>
+
+          <div className="quick-links">
+            <a href="#projects" className="project-link">Projects</a>
+            <a href="#contact" className="contact-link">Contact</a>
+          </div>
+
+          <div className="social-links">
+            <a 
+              href="https://github.com/biswajitmanna" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            
+            <a 
+              href="https://www.linkedin.com/in/biswajit-manna-063908237" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            
+            <a 
+              href="https://twitter.com/biswajitmanna77" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+          </div>
+        </div>
+
+        <div className="profile-pic">
+          <img src="/MyPhoto.jpg" alt="Profile Picture" />
+        </div>
+      </section>
+    </div>
   )
 }
 
